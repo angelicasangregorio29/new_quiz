f5dce62e3e43c95b34498c87fbf44d5cad0d3552
angel <angelica29sangregorio@gmail.com>
2025-12-04 14:22:40 +0100

    start quiz

---
+++ main.py
@@ -0,0 +1,13 @@
+# Lista di domande e risposte
+lista_domande = [
+    {"testo": "Qual è la capitale della Francia?", "risposta": "parigi"},
+    {"testo": "Qual è la capitale dell'Italia?", "risposta": "roma"},
+    {"testo": "Qual è la capitale della Spagna?", "risposta": "madrid"},
+    {"testo": "Qual è la capitale della Germania?", "risposta": "berlino"},
+    {"testo": "Qual è la capitale del Portogallo?", "risposta": "lisbona"}
+]
+
+lista_domande_length = len(lista_domande)
+counter_domanda_corrente = 0
+risultato_finale = []  # lista delle risposte date
+
